c2b611e59648c60fda2010bd0b10114d9391ea60
mielisi <a.melis003@gmail.com>
2023-11-14 09:54:09 +0100

    WIP-Initiated the conversion

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import Card from '../Ui/Card/Card';
+import {Card} from "../Ui/Card/Card";
 
-import classes from "./Settings.module.css"
+import classes from "./Settings.module.css";
 
 const Settings = () => {
-    const history = useHistory()
+  const history = {
+    push: (url: string) => {},
+  };
 
-    const urlSettingHandler = () =>{
-        history.push('/home/settings/gestione-url')
-    }
+  const urlSettingHandler = () => {
+    history.push("/home/settings/gestione-url");
+  };
 
-    return (
-        <Card className={classes.settings}>
-            <ul className={classes.settingsList}>
-                <li className={classes.settingsItem}>
-                    <p>Modifica/Elimina l'URL del DB</p>
-                    
-                    <button onClick={urlSettingHandler}>
-                        Gestisci l'URL
-                    </button>
-                </li>
-            </ul>
-        </Card>
-    );
-}
+  return (
+    <Card className={classes.settings}>
+      <ul className={classes.settingsList}>
+        <li className={classes.settingsItem}>
+          <p>Modifica/Elimina l'URL del DB</p>
+
+          <button onClick={urlSettingHandler}>Gestisci l'URL</button>
+        </li>
+      </ul>
+    </Card>
+  );
+};
 
 export default Settings;
--- src/components/Ui/Card/Card.tsx
+++ src/components/Ui/Card/Card.tsx
@@ -1,9 +1,6 @@
 import classes from "./Card.module.css";
 
-import React from "react";
 
-const Card = (props) => {
+export const Card = (props) => {
   return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
 };
-
-export default Card;
